20bc539e81ec42734094046bc15eccf35cac7173
Emil Fenenko <emil.fenenko@icefire.ee>
2020-02-18 09:24:34 +0200

    added local server file

--- src/server/util/TaskRunner.java
+++ src/server/util/TaskRunner.java
@@ -3,11 +3,16 @@ package server.util;
 import server.Server;
 import server.dto.ServersInfo;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.ConnectException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.TimerTask;
@@ -35,21 +40,46 @@ public class TaskRunner extends TimerTask {
                 .GET()
                 .uri(URI.create(catalogServer))
                 .build();
-        List<ServersInfo> ret = new LinkedList<>();
         try {
             String response = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
-            String[] availableServers = response.split("\n");
-            for (String server: availableServers) {
-                String[] serverWithPort = server.split("\t");
-                ret.add(new ServersInfo(serverWithPort[0], serverWithPort[1], true));
+            updateServers(response);
+        } catch (IOException | InterruptedException e) {
+            try {
+                readFromFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
-            String serversAsString = ret.stream().map(ServersInfo::getIpWithPort).collect(Collectors.joining(", "));
-            logger.log(Level.INFO, String.format("Available servers: %s", serversAsString));
-            Server.setAvailableServers(ret);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
+
+
+    }
+
+    private void updateServers(String response) throws IOException {
+        List<ServersInfo> ret = new LinkedList<>();
+        String[] availableServers = response.split("\n");
+        for (String server: availableServers) {
+            String[] serverWithPort = server.split(":");
+            ret.add(new ServersInfo(serverWithPort[0], serverWithPort[1], true));
+        }
+        String serversAsString = ret.stream().map(ServersInfo::getIpWithPort).collect(Collectors.joining(", "));
+        logger.log(Level.INFO, String.format("Available servers: %s", serversAsString));
+        Server.setAvailableServers(ret);
+        writeToFile(response);
+    }
+
+    private void writeToFile(String input) throws IOException {
+        File serverFile = new File("resources/servers.txt");
+        Path path = Paths.get(serverFile.getAbsolutePath());
+        serverFile.createNewFile();
+        try (BufferedWriter writer = Files.newBufferedWriter(path))
+        {
+            writer.write(input);
+        }
+    }
+
+    private void readFromFile() throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("resources/servers.txt"), StandardCharsets.UTF_8);
+        String response = lines.stream().collect(Collectors.joining("\n"));
+        updateServers(response);
     }
 }
